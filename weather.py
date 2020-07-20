a4bb236bd185fd254e89176e538ff7904d8170a3
Borys Vengerov <borys.vengerov@gmail.com>
2020-07-20 15:21:46 +0200

    Refactorings and stuff

--- weather.py
+++ weather.py
@@ -1,29 +1,19 @@
-"""WEATHER APP
+import sys
 
-Usage:
-    weather.py <address>
-
-Options:
-    <address> Place to search weather for.
-"""
-
-from docopt import docopt
-from pip._vendor import requests
+import requests
 
 
 def search_weather(address):
     print("You're searching the weather in {}.".format(address))
     import urllib.parse
 
-    url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(address) + '?format=json'
-
+    url = 'https://nominatim.openstreetmap.org/search/{}?format=json'.format(urllib.parse.quote(address))
     response = requests.get(url).json()
-    latitude = response[0]["lat"]
-    longitude = response[0]["lon"]
+    latitude, longitude = response[0]["lat"], response[0]["lon"]
 
     # Getting location for current position
     # r = requests.get('https://api.ipdata.co?api-key=test').json()
-    # print(r)
+    # latitude, longitude = r['latitude'], r['longitude']
 
     result = requests.get('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={}&lon={}'.format(
         latitude, longitude
@@ -32,8 +22,4 @@ def search_weather(address):
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='0.0.1')
-    if arguments['<address>']:
-        search_weather(address=arguments['<address>'])
-    else:
-        print(arguments)
+    search_weather(sys.argv[1])
