58ccfbcd28c2391d65978b727e7c24b9fdbbe17a
Borys Vengerov <borys.vengerov@gmail.com>
2020-07-26 22:55:29 +0200

    Refactorings + searching for weather by client ip when no address given

--- weather.py
+++ weather.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python3
-
+import json
 import sys
 import urllib.parse
 import requests
 
 
-def get_weather(address):
+def get_coordinates(address: str) -> tuple:
+    if not address:
+        print('Getting location for current position')
+        r = requests.get('https://api.ipdata.co?api-key=test').json()
+        latitude, longitude = r['latitude'], r['longitude']
+    else:
+        url = 'https://nominatim.openstreetmap.org/search/{}?format=json'.format(urllib.parse.quote(address))
+        response = requests.get(url).json()
+        latitude, longitude = response[0]["lat"], response[0]["lon"]
+    return round(float(latitude), 4), round(float(longitude), 4)
+
+
+def parse_current_weather(results):
+    print(results)
+
+
+def get_weather(address: str):
     """GET WEATHER APP"""
 
-    url = 'https://nominatim.openstreetmap.org/search/{}?format=json'.format(urllib.parse.quote(address))
-    response = requests.get(url).json()
-    latitude, longitude = response[0]["lat"], response[0]["lon"]
-
-    # Getting location for current position
-    # r = requests.get('https://api.ipdata.co?api-key=test').json()
-    # latitude, longitude = r['latitude'], r['longitude']
-
     result = requests.get('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={}&lon={}'.format(
-        latitude, longitude
+        *get_coordinates(address)
     ))
-    print(result.json())
+
+    print(parse_current_weather(result.json()))
+    # print(parse_close_weather(result.json()))
+    # print(parse_farther_weather(result.json()))
 
 
 if __name__ == '__main__':
-    get_weather(sys.argv[1])
+    try:
+        address = sys.argv[1]
+    except IndexError:
+        address = ''
+    get_weather(address)
